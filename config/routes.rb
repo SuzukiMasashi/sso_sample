0db6f02c4828b97030fd06047eeb184048173564
SuzukiMasashi <suzuki_masasi@nexway.co.jp>
2018-01-09 20:14:29 +0900

    :sparkles: Add sso by salesforce

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+
+  get "home", to: "home#index", as: "user_root"
+  root 'home#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
